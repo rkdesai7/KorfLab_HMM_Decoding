64801bca2758cb2f4219218880051a79c9530ead
Riya Desai <107080256+rkdesai7@users.noreply.github.com>
2024-07-09 16:08:09 -0700

    Update viterbi.py

--- HMM_Decoding/Scripts/viterbi.py
+++ HMM_Decoding/Scripts/viterbi.py
@@ -12,13 +12,15 @@ def log(transition_probs):
         log_transition_probs[from_state] = log_to_probs
     return log_transition_probs
 
-#read in fasta file
-sequence = 'GTATGATTTTTTAAAATTATAATTGTTTCTTTCGAAAAAAAAAATTTCATTTACAG'
 
-#load json
+
+#Initialization
+
+#read in fasta file and json
+sequence = 'GTATGATTTTTTAAAATTATAATTGTTTCTTTCGAAAAAAAAAATTTCATTTACAG'
 with open("C:/Users/riyak/Documents/Honors_Thesis/HMMs/HMM_AlternativeSplicing.json", 'r') as f:
     data = json.load(f)
-
+    
 #get values from json in log-scale
 o = data["order"][0] #order
 states = data["states"]
@@ -31,6 +33,8 @@ p = math.log(1/len(states)) #initial state probability for index 0
 for i in range(len(states)):
     matrix.append([(p, -1)])
 temp_emit = .25
+
+
 
 #fill matrix
 n = 1
@@ -50,6 +54,8 @@ for i in range(0, len(sequence)):
             matrix[j].append((max(calc), calc.index(max(calc))))      
     n+=1
 
+
+
 #Run viterbi
 length = len(sequence)
 output = [] #to store decoding
@@ -62,7 +68,6 @@ num = calc.index(max(calc))
 state = states[num]
 prob = matrix[num][length][0]
 prev_state = matrix[num][length][1]
-   
 output.append((sequence[length-1], state, prob))
 
 #run for rest of bases
@@ -78,7 +83,9 @@ for i in range(1, length):
     
     #update traceback state
     prev_state = matrix[prev_state][(i+1)*-1][1]
-    
-#reverse output so it displays in order
+
+
+
+#Display Results
 output = output[::-1]
 print(output)
