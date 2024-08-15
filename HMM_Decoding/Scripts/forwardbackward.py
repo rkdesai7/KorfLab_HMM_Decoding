b71d045686262b4e299d88c619bec01ad88b4fe4
rkdesai7 <rykdesai@ucdavis.edu>
2024-08-14 17:08:45 -0700

    Yuh

--- HMM_Decoding/Scripts/forwardbackward.py
+++ HMM_Decoding/Scripts/forwardbackward.py
@@ -29,8 +29,9 @@ def log(transition_probs):
         log_transition_probs[from_state] = log_to_probs
     return log_transition_probs
 def filter_sequence(seq):
+    valid = ['A', 'G', 'C', 'T']
     for i in seq:
-        if i != ('A' or 'G' or 'C' or 'T'):
+        if i not in valid:
             print("Sequence has an incorrect format and must only contain nucleotides (A, C, G, or T).")
             sys.exit()
     return True
@@ -73,7 +74,7 @@ for i in states:
 #Forward Fill
 #initialize matrix and emmissions
 forward_matrix = []
-p = math.log(1/len(states)) #initial state probability for index 0
+p = 1/len(states) #initial state probability for index 0
 for i in range(len(states)):
     forward_matrix.append([p])
 temp_emit = math.log(.25)
@@ -83,10 +84,17 @@ for i in range(1, len(sequence)+1):
         sum = 0
         for k in range(len(states)):
             if i < orders[j]+1:
-                sum += forward_matrix[k][i-1]+transitions[states[k]][states[j]]+temp_emit
+                prev_prob = math.log(forward_matrix[k][i-1])
+                prob = prev_prob+transitions[states[k]][states[j]]+temp_emit
+                sum += math.exp(prob)
             else:
                 seq = sequence[i-orders[j]-1: i]
-                sum += forward_matrix[k][i-1]+transitions[states[k]][states[j]]+emits[states[j]][seq]
+                try:
+                    prev_prob = math.log(forward_matrix[k][i-1])
+                except:
+                    print(forward_matrix[k][i-1])
+                prob = prev_prob+transitions[states[k]][states[j]]+emits[states[j]][seq]
+                sum += math.exp(prob)
         forward_matrix[j].append(sum)
 for i in forward_matrix:
     i = i.pop(0)
@@ -103,16 +111,20 @@ for i in range(len(sequence), -1, -1):
         continue
     elif (i+1)==len(sequence):
         for j in backward_matrix:
-            j[i] = 0
+            j[i] = 1
     else:
         for j in range(len(states)):
             sum = 0
             for k in range(len(states)):
                 if (i+1) < (orders[k]+1):
-                    sum += transitions[states[j]][states[k]]+temp_emit+backward_matrix[k][i+1]
+                    prev = math.log(backward_matrix[k][i+1])
+                    prob = transitions[states[j]][states[k]]+prev+temp_emit
+                    sum += math.exp(prob)
                 else:
                     seq = sequence[i-(orders[k]-1):i+2]
-                    sum += transitions[states[j]][states[k]]+backward_matrix[k][i+1]+emits[states[k]][seq]
+                    prev = math.log(backward_matrix[k][i+1])
+                    prob = transitions[states[j]][states[k]]+prev+emits[states[k]][seq]
+                    sum += math.exp(prob)
             backward_matrix[j][i] = sum
 
 #Decode
@@ -123,11 +135,10 @@ for i in range(len(states)):
 for i in range(len(forward_matrix[0])):
     denominator = 0
     for k in range(len(states)):
-        denominator += forward_matrix[k][i]+backward_matrix[k][i]
-        print(i, denominator)
+        denominator += forward_matrix[k][i]*backward_matrix[k][i]
     for j in range(len(states)):
-        numerator = forward_matrix[j][i]+backward_matrix[j][i]
-        prob = numerator-denominator
+        numerator = forward_matrix[j][i]*backward_matrix[j][i]
+        prob = numerator/denominator
         true_probs[j].append(prob)
 
 #Graph?
@@ -139,7 +150,6 @@ state_name = "Probability that each position is in state " + arg.state
 plt.title(state_name)
 plt.xlabel("Position")
 plt.ylabel("Probability")
-print(arg.state)
 plt.show()
 
 #Find the maximums
